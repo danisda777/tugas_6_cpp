7aac2cec0fb28eb97a5f8ef6b2e1691f8cd51026
Sahrul Dani Ardiansyah <daniardiansyah448@gmail.com>
2023-01-08 10:18:19 +0700

    Tugas 6 C++
    Dikirim untuk Niomic

---
+++ Tugas6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int x,z;
+	cout<<"Inputkan Nilai X = ";
+	cin>>x;
+	cout<<"Inputkan Nilai Z = ";
+	cin>>z;
+	if (x>=z){
+	cout<<"Hasil : "<<x+z;
+	}else {
+	cout<<"Hasil : "<<x-z;
+	}
+}
